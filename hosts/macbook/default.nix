4d58870c7a4cae8e26443457e75a2c6d167a0b43
Jacob Daitzman <jdtzmn@gmail.com>
2025-11-24 15:37:57 -0500

    Install kap screen recorder

--- hosts/macbook/default.nix
+++ hosts/macbook/default.nix
@@ -1,156 +1,158 @@
-{ pkgs, ... }: {
-    # Disable nix because we're using Determinate Nix
-    nix.enable = false;
+{ pkgs, ... }:
+{
+  # Disable nix because we're using Determinate Nix
+  nix.enable = false;
 
-    # Set state version
-    system.stateVersion = "25.05";
+  # Set state version
+  system.stateVersion = "25.05";
 
-    ##############################
-    # Shell
-    ##############################
+  ##############################
+  # Shell
+  ##############################
 
-    environment.shells = [ pkgs.fish ];
+  environment.shells = [ pkgs.fish ];
 
-    # Yes, you do need to enable fish in nix-darwin and home-manager.. sigh
-    programs.fish.enable = true;
+  # Yes, you do need to enable fish in nix-darwin and home-manager.. sigh
+  programs.fish.enable = true;
 
-    ##############################
-    # User
-    ##############################
+  ##############################
+  # User
+  ##############################
 
-    users.users.jacob = {
-        name = "jacob";
-        home = "/Users/jacob";
-        shell = pkgs.fish;
-        ignoreShellProgramCheck = true;
-    };
+  users.users.jacob = {
+    name = "jacob";
+    home = "/Users/jacob";
+    shell = pkgs.fish;
+    ignoreShellProgramCheck = true;
+  };
 
-    system.primaryUser = "jacob";
+  system.primaryUser = "jacob";
 
-    ##############################
-    # System Defaults
-    ##############################
+  ##############################
+  # System Defaults
+  ##############################
 
-    # Dock
-    system.defaults.dock.autohide = true;
+  # Dock
+  system.defaults.dock.autohide = true;
 
-    # Desktop
-    # Disable standard click to show desktop
-    system.defaults.WindowManager.EnableStandardClickToShowDesktop = false;
+  # Desktop
+  # Disable standard click to show desktop
+  system.defaults.WindowManager.EnableStandardClickToShowDesktop = false;
 
-    # Windows
-    # Disable window margins (set window resize increments to 0)
-    system.defaults.WindowManager.EnableTiledWindowMargins = false;
+  # Windows
+  # Disable window margins (set window resize increments to 0)
+  system.defaults.WindowManager.EnableTiledWindowMargins = false;
 
-    # Disable cmd + space for Spotlight (requires restart)
-    system.defaults.CustomUserPreferences = {
-        "com.apple.symbolichotkeys" = {
-          AppleSymbolicHotKeys = {
-            # 64 is the configuration entry for Spotlight search
-            "64" = {
-                enabled = false;
-            };
-          };
+  # Disable cmd + space for Spotlight (requires restart)
+  system.defaults.CustomUserPreferences = {
+    "com.apple.symbolichotkeys" = {
+      AppleSymbolicHotKeys = {
+        # 64 is the configuration entry for Spotlight search
+        "64" = {
+          enabled = false;
         };
-        # Figure out a way to disable ctrl+space for "Select the previous input source" so that Zed can show completions
+      };
     };
+    # Figure out a way to disable ctrl+space for "Select the previous input source" so that Zed can show completions
+  };
 
-    ##############################
-    # Home Manager
-    ##############################
+  ##############################
+  # Home Manager
+  ##############################
 
-    home-manager.useGlobalPkgs = true;
-    home-manager.useUserPackages = true;
-    home-manager.users.jacob = {
-        imports = [ ./home.nix ];
-    };
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  home-manager.users.jacob = {
+    imports = [ ./home.nix ];
+  };
 
-    ##############################
-    # Nix-Homebrew (installs homebrew)
-    ##############################
+  ##############################
+  # Nix-Homebrew (installs homebrew)
+  ##############################
 
-    nix-homebrew = {
-        # Install Homebrew under the default prefix
-        enable = true;
+  nix-homebrew = {
+    # Install Homebrew under the default prefix
+    enable = true;
 
-        # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-        enableRosetta = true;
+    # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+    enableRosetta = true;
 
-        # User owning the Homebrew prefix
-        user = "jacob";
-    };
+    # User owning the Homebrew prefix
+    user = "jacob";
+  };
 
-    ##############################
-    # Homebrew (installs dependencies)
-    ##############################
+  ##############################
+  # Homebrew (installs dependencies)
+  ##############################
 
-    homebrew = {
-        enable = true;
-        taps = [
-            "oven-sh/bun"
-        ];
-        brews = [
-            # Programming
-            "opencode"
-            "asdf"
-            "temporal"
-            "uv"
-            "bun"
-            "dagger/tap/dagger"
-        ];
-        casks = [
-            # Productivity
-            "bettertouchtool"
-            "alfred"
-            "alt-tab"
-            "homerow"
-            "nordvpn"
-            "zoom"
-            "betterdisplay"
-            "linear-linear"
+  homebrew = {
+    enable = true;
+    taps = [
+      "oven-sh/bun"
+    ];
+    brews = [
+      # Programming
+      "opencode"
+      "asdf"
+      "temporal"
+      "uv"
+      "bun"
+      "dagger/tap/dagger"
+    ];
+    casks = [
+      # Productivity
+      "bettertouchtool"
+      "alfred"
+      "alt-tab"
+      "homerow"
+      "nordvpn"
+      "zoom"
+      "betterdisplay"
+      "linear-linear"
+      "kap"
 
-            # AI Tools
-            # Manually install Chorus
+      # AI Tools
+      # Manually install Chorus
 
-            # Writing
-            "notion"
+      # Writing
+      "notion"
 
-            # Browser
-            "brave-browser"
+      # Browser
+      "brave-browser"
 
-            # Music
-            "spotify"
+      # Music
+      "spotify"
 
-            # Programming
-            "cursor"
-            "visual-studio-code"
-            "zed"
-            "wezterm"
-            "tower"
-            "tailscale-app"
-            "orbstack"
-            "conductor"
-            "bruno"
-            "dbeaver-community"
+      # Programming
+      "cursor"
+      "visual-studio-code"
+      "zed"
+      "wezterm"
+      "tower"
+      "tailscale-app"
+      "orbstack"
+      "conductor"
+      "bruno"
+      "dbeaver-community"
 
-            # Work
-            "slack"
-        ];
-    };
+      # Work
+      "slack"
+    ];
+  };
 
-    ##############################
-    # Fonts
-    ##############################
+  ##############################
+  # Fonts
+  ##############################
 
-    fonts = {
-        packages = [ pkgs.nerd-fonts.fira-code ];
-    };
+  fonts = {
+    packages = [ pkgs.nerd-fonts.fira-code ];
+  };
 
-    ##############################
-    # Install Rosetta 2 for Done.fish plugin.
-    ##############################
+  ##############################
+  # Install Rosetta 2 for Done.fish plugin.
+  ##############################
 
-    system.activationScripts.extraActivation.text = ''
-        softwareupdate --install-rosetta --agree-to-license
-    '';
+  system.activationScripts.extraActivation.text = ''
+    softwareupdate --install-rosetta --agree-to-license
+  '';
 }
