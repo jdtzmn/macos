45c6bf2a768f4628ab4b454489009ad7b384216e
Jacob Daitzman <jdtzmn@gmail.com>
2025-11-27 19:24:12 -0500

    Use transparent tokyonight for opencode

--- shared/opencode.nix
+++ shared/opencode.nix
@@ -2,4 +2,5 @@
 {
   home.file.".config/opencode/opencode.jsonc".source = ./config/opencode/opencode.jsonc;
   home.file.".config/opencode/plugin/notification.js".source = ./config/opencode/plugin/notification.js;
+  home.file.".config/opencode/themes/tokyonight-transparent.json".source = ./config/opencode/themes/tokyonight-transparent.json;
 }
