d208609a67315efee472f996a4e8a5ec758d8685
Jacob Daitzman <jdtzmn@gmail.com>
2025-12-19 00:12:44 -0500

    Enable single line pure prompt

--- shared/config/config.fish
+++ shared/config/config.fish
@@ -26,5 +26,8 @@ function github
     end
 end
 
+# Fish pure prompt
 # https://pure-fish.github.io/pure/#slowness-try-async-git-prompt
 set -g async_prompt_functions _pure_prompt_git
+
+set -g pure_enable_single_line_prompt true
