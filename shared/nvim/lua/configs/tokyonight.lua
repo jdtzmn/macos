d5e5d750f543cc9188287b8362107a33439133ca
Jacob Daitzman <jdtzmn@gmail.com>
2025-12-23 15:53:05 -0500

    Remove the tokyonight transparency and default transparency options

--- shared/nvim/lua/configs/tokyonight.lua
+++ shared/nvim/lua/configs/tokyonight.lua
@@ -1,9 +1,1 @@
-local options = {
-  transparent = vim.g.transparent_enabled,
-  styles = {
-    sidebars = "transparent",
-    floats = "transparent",
-  }
-}
-
-require('tokyonight').setup(options) 
+require('tokyonight').setup({}) 
