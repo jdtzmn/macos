628eadf4c2abeb5e9625b320a9d3da3abc375c7d
Jacob Daitzman <jdtzmn@gmail.com>
2026-02-17 10:00:08 -0500

    Increase wezterm max tab width

--- shared/config/wezterm.lua
+++ shared/config/wezterm.lua
@@ -104,6 +104,7 @@ config.use_fancy_tab_bar = false
 config.show_new_tab_button_in_tab_bar = false
 config.show_close_tab_button_in_tabs = false
 config.hide_tab_bar_if_only_one_tab = true
+config.tab_max_width = 48
 
 -- Customize tab bar colors
 config.colors = {
