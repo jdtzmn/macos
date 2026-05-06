22390afca0b964766b679b5a1c6ac72579b5aefd
Jacob Daitzman <jdtzmn@gmail.com>
2026-05-06 13:30:48 -0400

    Add daily docker prune systemd user timer on Linux

--- hosts/linux/home.nix
+++ hosts/linux/home.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   repoDir ? null,
   username ? "jacob",
   homeDirectory ? "/home/jacob",
@@ -32,4 +33,42 @@
         '';
     };
     programs.zsh.enable = false;
+
+    ##############################
+    # Scheduled maintenance
+    ##############################
+
+    # Daily Docker prune via systemd user timer.
+    # Note: user timers only run while the user has an active session unless
+    # `loginctl enable-linger <user>` has been set (one-time, manual).
+    systemd.user.services.docker-prune = {
+        Unit = {
+            Description = "Prune unused Docker resources";
+        };
+        Service = {
+            Type = "oneshot";
+            # Split into two steps: docker rejects `--volumes` together with
+            # the `until` filter, so we time-filter the system prune and then
+            # prune dangling volumes separately. systemd runs ExecStart entries
+            # in order under Type=oneshot.
+            ExecStart = [
+                "${pkgs.docker}/bin/docker system prune -af --filter until=168h"
+                "${pkgs.docker}/bin/docker volume prune -af"
+            ];
+        };
+    };
+
+    systemd.user.timers.docker-prune = {
+        Unit = {
+            Description = "Daily Docker prune timer";
+        };
+        Timer = {
+            OnCalendar = "daily";
+            Persistent = true;          # run on next boot if the machine was off
+            RandomizedDelaySec = "30m";
+        };
+        Install = {
+            WantedBy = [ "timers.target" ];
+        };
+    };
 }
